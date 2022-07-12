82b6af02015961e117693a946ca27236182c7ca6
paulohenriquepaulo <p.henrique461@gmail.com>
2022-07-12 00:11:30 -0300

    Logica_Aplicada

---
+++ Logica_Aplicada/If_Else.js
@@ -0,0 +1,9 @@
+let vingador = "Capitão América";
+
+if(vingador === "Homen de ferro"){
+    console.log("Sou melhor vingador")
+}else if(vingador === "Capitão América"){
+    console.log("Cadê meu escudo?")
+}else{
+    console.log("Hulk esmaga tudo!!")
+}
