e43280e85120f565b658bf2552b9638175a22add
paulohenriquepaulo <p.henrique461@gmail.com>
2022-07-16 22:09:50 -0300

    Condicional Avançado

---
+++ Logica_Aplicada/Boleano.js
@@ -0,0 +1,15 @@
+const vingador = 'Capitão América';
+
+let euSouMelhorVingador = vingador === 'Homem de Ferro';
+console.log(euSouMelhorVingador);
+
+let euTenhoUmEscudo = vingador === 'Capitão América';
+console.log(euTenhoUmEscudo);
+
+let soma = 10 + 10;
+
+let resultadoValido = soma > 20;
+console.log(resultadoValido);
+
+resultadoValido = soma >= 20;
+console.log(resultadoValido);
---
+++ Logica_Aplicada/Ternario.js
@@ -0,0 +1,12 @@
+let euSouUmaLenda = true;
+
+let quemSouEu = euSouUmaLenda ? 'Você é o Will Smith' : 'Você não é a lenda';
+
+console.log(quemSouEu);
+
+console.log('------------------------------');
+let minhaSenha = 100;
+let senhaAtual = 99;
+
+let suaVez = minhaSenha === senhaAtual ? 'É sua veze!' : 'Não é sua vez!';
+console.log(suaVez);
