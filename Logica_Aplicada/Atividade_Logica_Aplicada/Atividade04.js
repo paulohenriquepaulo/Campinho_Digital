cce7f6adf0f656f6902e17a7637f699beea32ca5
paulohenriquepaulo <p.henrique461@gmail.com>
2022-07-20 13:25:34 -0300

    Atividade 0

--- Logica_Aplicada/Atividade_Logica_Aplicada/Atividade04.js
+++ Logica_Aplicada/Atividade_Logica_Aplicada/Atividade04.js
@@ -1,9 +1,9 @@
 const rs =  require('readline-sync');
 
-const quantidadeMaca = (rs.question('Informe a quantidade de maças: '))
+const quantidadeMaca = (rs.question('Informe a quantidade de maças: '));
 
 if (quantidadeMaca < 12 ) {
-    console.log('O valor total é: ' + parseFloat(quantidadeMaca * 0.30))
+    console.log('O valor total é: ' + parseFloat(quantidadeMaca * 0.30));
 } else {
-    console.log('O valor total é: ' + parseFloat(quantidadeMaca * 0.25))
+    console.log('O valor total é: ' + parseFloat(quantidadeMaca * 0.25));
 }
